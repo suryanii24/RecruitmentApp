dbcb42af763f9ec28afa56d2848f0c1028a46cf4
suryanii24 <suryanirachma@gmail.com>
2026-06-04 17:12:32 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecruitmentApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
